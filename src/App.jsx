8cf5666961569515fd6af3642e194441bbfb5601
Alejandra Tobar <alejandratcag@gmail.com>
2024-03-05 11:47:33 -0300

    Actualizaciones

--- src/App.jsx
+++ src/App.jsx
@@ -9,53 +9,12 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
-//React useState y useEffect import
-import React, { useState, useEffect } from "react";
+//React useState import
+import React, { useState } from "react";
 
 const App = () => {
   //Estado para buscar feriado
   const [search, setSearch] = useState("");
-
-  //Estado para los feriados
-  const [holidays, setHolidays] = useState([]);
-  const [error, setError] = useState(null); //Estado para manejar los errores
-
-  //Hook useEffect para obtener datos de la API
-  useEffect(() => {
-
-    const getData = async () => {
-      
-      try {
-        //Sentencia Try
-        const response = await fetch(
-          "https://api.victorsanmartin.com/feriados/en.json"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const json = await response.json();
-        //Método sort: Ordena los feriados por fecha
-        const sortedHolidays = json.data.sort(
-          (a, b) => new Date(a.date) - new Date(b.date)
-        );
-        setHolidays(sortedHolidays); //Actualización del estado con los datos de la API ordenados
-
-      } catch (error) {
-        //Sentencia Catch
-        console.error("Error fetching data: ", error);
-        setError(error.message); //Actualización del estado de error
-      }
-    };
-
-    getData();
-  }, []); //Array vacío asegura que este efecto se ejecute sólo una vez después de la renderización
-
-  //Filtra titulo basado en término de búsqueda 'search'
-  const filteredHoliday = search
-    ? holidays.filter((holiday) =>
-        holiday.title.toLowerCase().includes(search.toLowerCase())
-      )
-    : holidays;
 
   return (
     <>
@@ -78,7 +37,7 @@ const App = () => {
           <Card.Title>DÍAS FERIADOS EN CHILE 2024</Card.Title>
           <Buscador setSearch={setSearch} />
           <Card.Text>
-            <MiApi holidays={filteredHoliday} />
+            <MiApi search={search} />
           </Card.Text>
         </Card.Body>
       </Card>
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,7 +1,47 @@
-import React from "react";
 import Table from "react-bootstrap/Table";
+import React, { useState, useEffect } from "react";
 
-const MiApi = ({ holidays }) => {
+
+const MiApi = ({ search = "" }) => {
+  //Estado para los feriados
+  const [holidays, setHolidays] = useState([]);
+  const [error, setError] = useState(null); //Estado para manejar los errores
+
+  //Hook useEffect para obtener datos de la API
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        //Sentencia Try
+        const response = await fetch(
+          "https://api.victorsanmartin.com/feriados/en.json"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+
+        //Método sort: Ordena los feriados por fecha
+        const sortedHolidays = json.data.sort(
+          (a, b) => new Date(a.date) - new Date(b.date)
+        );
+        setHolidays(sortedHolidays); //Actualización del estado con los datos de la API ordenados
+      } catch (error) {
+        //Sentencia Catch
+        console.error("Error fetching data: ", error);
+        setError(error.message); //Actualización del estado de error
+      }
+    };
+
+    getData();
+  }, []); //Array vacío asegura que este efecto se ejecute sólo una vez después de la renderización
+
+  //Filtra titulo basado en término de búsqueda 'search'
+  const filteredHoliday = search
+    ? holidays.filter((holiday) =>
+        holiday.title.toLowerCase().includes(search.toLowerCase())
+      )
+    : holidays;
+
   return (
     <Table responsive striped bordered hover>
       <thead>
@@ -15,7 +55,7 @@ const MiApi = ({ holidays }) => {
       </thead>
       <tbody>
         {/*Itera sobre arreglo de monedas para generar fila por cada uno*/}
-        {holidays.map((holiday, index) => (
+        {filteredHoliday.map((holiday, index) => (
           <tr key={holiday}>
             <td>{index + 1}</td>
             <td>{holiday.date}</td>
